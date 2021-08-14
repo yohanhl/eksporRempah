ff254a027041f0d1d3118a45b6267e1387e82eb2
yohanhl <yohannl030@gmail.com>
2021-08-14 10:16:50 +0700

    14082021

--- js/main.js
+++ js/main.js
@@ -370,3 +370,23 @@ bxslider
     animation: "fade",
   });
 })(jQuery);
+
+// Modal Setup
+var modal = document.getElementById('modal');
+
+var modalClose = document.getElementById('modal-close');
+modalClose.addEventListener('click', function() { 
+  modal.style.display = "none";
+});
+
+// global handler
+document.addEventListener('click', function (e) { 
+  if (e.target.className.indexOf('modal-target') !== -1) {
+      var img = e.target;
+      var modalImg = document.getElementById("modal-content");
+      var captionText = document.getElementById("modal-caption");
+      modal.style.display = "block";
+      modalImg.src = img.src;
+      captionText.innerHTML = img.alt;
+   }
+});
